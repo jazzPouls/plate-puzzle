aaa4b45d0bd3939b5078dc407672faaeebf62a74
jazzPouls <jazzpouls@icloud.com>
2021-02-03 14:47:50 -0800

    PG got own file

---
+++ PuzzleGenerator.js
@@ -0,0 +1,66 @@
+var fs = require('fs');
+
+module.exports = function PuzzleGenerator() {
+    this.puzzle = null;
+    this.template = '1AAA111';
+    var dict = '';
+    fs.readFile(__dirname + '/public/dict/dictionary.txt', 'utf8', function(err, data) {
+      if (err) throw err;
+      dict = data
+    });
+
+    this.newPuzzle = function(template) {
+        if (template) {
+            this.template = template
+        }
+        var lic = this.generateRandomLicense();
+        this.puzzle = {
+            license:lic,
+            solution:solvePuzzle(lic)
+        }
+        return this.puzzle
+
+    };
+
+    this.solveCustomPuzzle = function(custom_license) {
+        this.puzzle = {
+            license: custom_license,
+            solution: solvePuzzle(custom_license)
+        }
+        return this.puzzle
+
+    }
+
+    var solvePuzzle = function(license) {
+        var rgx = '\\b' + license.replace(/[0-9]/g,'').replace(/(\w)/g,'$1\\w*') + '\\b';
+        var res = dict.match(new RegExp(rgx, 'g'));
+        var solution_type = 'strict';
+        if (!res) {                                         // if not strict solution, allow leading characters
+                                                            // e.g. XBM => EXHIBITIONISM
+            var rgx_loose = '\\b\\w*' + license.replace(/[0-9]/g,'').replace(/(\w)/g,'$1\\w*') + '\\b';
+            var res_loose = dict.match(new RegExp(rgx_loose, 'g'));
+            if (!res_loose) {
+                return {solutions: ['No solution'], solution_type: null};
+            }
+            solution_type = 'loose'
+            res = res_loose
+        }
+        res.sort(function(a,b) {return a.length-b.length});
+        return {solutions: res, solution_type: solution_type}
+    }
+
+    this.generateRandomLicense = function() {
+        var letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        var license = '';
+        for (let i = 0; i<this.template.length; i++) {
+            if (this.template.charAt(i).isletter()) {
+                var seed = Math.floor(Math.random()*letters.length);
+                license += letters[seed];
+            } else if (this.template.charAt(i).isdigit()) {
+                license += Math.floor(Math.random()*10);
+            }
+        }
+        return license
+    }
+
+}
--- app.js
+++ app.js
@@ -7,78 +7,12 @@
 // sort guesses by length
 
 var express = require('express'); // Express web server framework
-var fs = require('fs');
-
-function PuzzleGenerator() {
-    this.puzzle = null;
-    this.template = '1AAA111';
-
-    this.newPuzzle = function(template) {
-        if (template) {
-            this.template = template
-        }
-        var lic = this.generateRandomLicense();
-        this.puzzle = {
-            license:lic,
-            solution:solvePuzzle(lic)
-        }
-        return this.puzzle
-
-    };
-
-    this.solveCustomPuzzle = function(custom_license) {
-        this.puzzle = {
-            license: custom_license,
-            solution: solvePuzzle(custom_license)
-        }
-        return this.puzzle
-
-    }
-
-    var solvePuzzle = function(license) {
-        var rgx = '\\b' + license.replace(/[0-9]/g,'').replace(/(\w)/g,'$1\\w*') + '\\b';
-        var res = raw_dict.match(new RegExp(rgx, 'g'));
-        var solution_type = 'strict';
-        if (!res) {                                         // if not strict solution, allow leading characters
-                                                            // e.g. XBM => EXHIBITIONISM
-            var rgx_loose = '\\b\\w*' + license.replace(/[0-9]/g,'').replace(/(\w)/g,'$1\\w*') + '\\b';
-            var res_loose = raw_dict.match(new RegExp(rgx_loose, 'g'));
-            if (!res_loose) {
-                return {solutions: ['No solution'], solution_type: null};
-            }
-            solution_type = 'loose'
-            res = res_loose
-        }
-        res.sort(function(a,b) {return a.length-b.length});
-        return {solutions: res, solution_type: solution_type}
-    }
-
-    this.generateRandomLicense = function() {
-        var letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        var license = '';
-        for (let i = 0; i<this.template.length; i++) {
-            if (this.template.charAt(i).isletter()) {
-                var seed = Math.floor(Math.random()*letters.length);
-                license += letters[seed];
-            } else if (this.template.charAt(i).isdigit()) {
-                license += Math.floor(Math.random()*10);
-            }
-        }
-        return license
-    }
-
-}
-
-var puzzleGenerator = new PuzzleGenerator();
+var PuzzleGenerator = require('./PuzzleGenerator.js')
 
 var app = express()
 app.use(express.static(__dirname + '/public'))
 
-var raw_dict = '';
-fs.readFile(__dirname + '/public/dict/dictionary.txt', 'utf8', function(err, data) {
-  if (err) throw err;
-  raw_dict = data
-});
+var puzzleGenerator = new PuzzleGenerator();
 
 app.get('/puzzle', (req,res) => {
     res.send(puzzleGenerator.puzzle)
@@ -92,7 +26,10 @@ app.get('/solvecustom', (req,res) => {
     res.send(puzzleGenerator.solveCustomPuzzle(req.query.custom_license));
 });
 
-app.listen(8888)
+const port = process.env.PORT || 8888;
+const listener = app.listen(port, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
 
 String.prototype.isletter = function() {
     return ((this >= 'A') && (this <= 'Z'));
